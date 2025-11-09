4b4361b3d360585527e2dd8c1fc1a7cd6cbaddb5
veepforcheap90 <154490466+veepforcheap90@users.noreply.github.com>
2025-11-10 01:05:28 +0330

    Add files via upload

---
+++ yyds/sniarbtej/ﹼ.java
@@ -0,0 +1,15 @@
+package yyds.sniarbtej;
+
+final class ﹼ implements ʿн<T> {
+  ﹼ(ˍʶ paramˍʶ) {}
+  
+  public final T ʿᵉ() {
+    return (T)new ᐝʶ<>();
+  }
+}
+
+
+/* Location:              C:\Users\kb\Downloads\ja-netfilter.jar!\yyds\sniarbtej\ﹼ.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
